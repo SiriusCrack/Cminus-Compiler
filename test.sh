37043642effcbe93434dc30d83b4a85daa9808d4
SiriusCrack <MorganBrockman1998@Gmail.com>
2022-09-28 14:31:20 -0700

    made working test script

--- test.sh
+++ test.sh
@@ -2,5 +2,8 @@ TESTBATCH=testDataA2
 
 for i in ./TestData/$TESTBATCH/*.c-; do
 	echo $i
-	./src/c- -p $i
+	#j=${i##*/}
+	j=${i%.c-}
+	j=$j.out
+	diff -rs <(./src/c- -p $i) $j | egrep '^Files .+ and .+ are identical$' | awk -F '(Files | and | are identical)' '{print "MATCH"}'
 done
