eed2d55738e2bbcda7a415bdbab6baa40d356f58
SiriusCrack <MorganBrockman1998@Gmail.com>
2022-10-13 22:32:58 -0700

    reworked the fuck outta WriteRefs() and SemanticAnalysis.c in general

--- src/SemanticAnalysis.c
+++ src/SemanticAnalysis.c
@@ -7,15 +7,15 @@ extern ScopeTable * SymbolTable;
 extern int warns;
 extern int errs;
 
-DataType CmpHandler (Node * tree, ScopeTable * table);
-DataType UnaryCmpHandler (Node * tree, ScopeTable * table);
-DataType RelOpHandler (Node * tree, ScopeTable * table);
-DataType OpHandler (Node * tree, ScopeTable * table);
-DataType UnaryHandler (Node * tree, ScopeTable * table);
-DataType ConstHandler (Node * tree, ScopeTable * table);
-char * DataTypeToString (DataType dataType);
 int IsScope (Node * node);
+int IsNewEntry (Node * node);
+int IsOp (Node * node);
+int IsCmp (Node * node);
+int IsUnaryCmp (Node * node);
+int IsRelOp (Node * node);
 int IsUnary (Node * node);
+int IsArrAd (Node * node);
+int IsConst (Node * node);
 
 void WriteScopes (Node * node, ScopeTable * table) {
     // Action
@@ -39,22 +39,97 @@ void WriteScopes (Node * node, ScopeTable * table) {
     }
 }
 
-void WriteRefs (Node * tree, ScopeTable * table) {
+DataType WriteRefs (Node * tree, ScopeTable * table) {
+    DataType dataType = unknown;
     ScopeTable * newScope = table;
     if(IsScope(tree)) {
         newScope = GetMatchingChildScope(newScope, tree->UID);
     }
-    // Action
-    if(tree->nodeType == ntOp || tree->nodeType == ntTrueAssign) {
-        OpHandler(tree, newScope);
-    } else if(tree->nodeType == ntOrOp || tree->nodeType == ntAndOp) {
-        CmpHandler(tree, newScope);
-    } else {
-        if(tree->nodeType == ntID || tree->nodeType == ntCall) {
-            SymbolTableEntry * newEntry = NewEntry(tree);
-            AddEntryToScope(newEntry, newScope);
+    if(IsOp(tree)) {
+        DataType dataTypeChildren[2] = {unknown, unknown};
+        dataTypeChildren[0] = WriteRefs(tree->child[0], newScope);
+        dataTypeChildren[1] = WriteRefs(tree->child[1], newScope);
+        if(dataTypeChildren[0] != unknown && dataTypeChildren[1] != unknown) { // seems wrong?
+            if(dataTypeChildren[0] != dataTypeChildren[1]) {
+                errs = errs + 1;
+                printf(
+                    "ERROR(%d): '%s' requires operands of the same type but lhs is type %s and rhs is type %s.\n",
+                    tree->lineNum,
+                    tree->literal,
+                    DataTypeToString(dataTypeChildren[0]),
+                    DataTypeToString(dataTypeChildren[1])
+                );
+            }
         }
-    // Traversal
+        tree->dataType = dataTypeChildren[0];
+        dataType = dataTypeChildren[0];
+    } else if(IsCmp(tree)) {
+        DataType dataTypeChildren[2] = {unknown, unknown};
+        dataTypeChildren[0] = WriteRefs(tree->child[0], newScope);
+        dataTypeChildren[1] = WriteRefs(tree->child[1], newScope);
+        if(dataTypeChildren[0] != dataTypeChildren[1]) {
+            errs = errs + 1;
+            printf(
+                "ERROR(%d): '%s' requires operands of the same type but lhs is type %s and rhs is type %s.\n",
+                tree->lineNum,
+                tree->literal,
+                DataTypeToString(dataTypeChildren[0]),
+                DataTypeToString(dataTypeChildren[1])
+            );
+        }
+        tree->dataType = boolData;
+        dataType = boolData;
+    } else if(IsUnaryCmp(tree)) {
+        DataType dataTypeChild = unknown;
+        dataTypeChild = WriteRefs(tree->child[0], newScope);
+        //error?
+        tree->dataType = boolData;
+        dataType = boolData;
+    } else if(IsRelOp(tree)) {
+        DataType dataTypeChildren[2] = {unknown, unknown};
+        dataTypeChildren[0] = WriteRefs(tree->child[0], newScope);
+        dataTypeChildren[1] = WriteRefs(tree->child[1], newScope);
+        if(dataTypeChildren[0] != dataTypeChildren[1]) {
+            errs = errs + 1;
+            printf(
+                "ERROR(%d): '%s' requires operands of the same type but lhs is type %s and rhs is type %s.\n",
+                tree->lineNum,
+                tree->literal,
+                DataTypeToString(dataTypeChildren[0]),
+                DataTypeToString(dataTypeChildren[1])
+            );
+        }
+        tree->dataType = boolData;
+        dataType = boolData;
+    } else if(IsUnary(tree)) {
+        DataType dataTypeChild = unknown;
+        dataTypeChild = WriteRefs(tree->child[0], newScope);
+        //error?
+        tree->dataType = dataTypeChild;
+        dataType = dataTypeChild;
+    } else {
+        if(IsConst(tree)) {
+            switch (tree->nodeType) {
+                case ntNumConst:
+                    dataType = intData;
+                    break;
+                case ntCharConst:
+                    dataType = charData;
+                    break;
+                case ntBoolConst:
+                    dataType = boolData;
+                    break;
+                default:
+                    printf("uhhhhhhhhhhhh %s\n", tree->literal);
+                    break;
+            }
+            tree->dataType = dataType;
+        } else if(IsNewEntry(tree)) {
+            SymbolTableEntry * newEntry = NewEntry(tree);
+            if(AddEntryToScope(newEntry, newScope)) {
+                dataType = newEntry->following->node->dataType;
+            }
+        }
         int i;
         for(i = 0; i < AST_MAX_CHILDREN; i++) {
             if(tree->child[i] != NULL) {
@@ -65,6 +140,7 @@ void WriteRefs (Node * tree, ScopeTable * table) {
     if(tree->sibling != NULL) {
         WriteRefs(tree->sibling, table);
     }
+    return dataType;
 }
 
 void CheckMain (ScopeTable *table) {
@@ -87,6 +163,7 @@ void CheckUse (ScopeTable *table) {
     while(cur != NULL) {
         if(cur->isDecl) {
             if(cur->followers[0] == NULL) {
+                warns = warns + 1;
                 printf(
                     "WARNING(%d): The variable '%s' seems not to be used.\n",
                     cur->node->lineNum,
@@ -106,160 +183,65 @@ void CheckUse (ScopeTable *table) {
     }
 }
 
-DataType CmpHandler (Node * tree, ScopeTable * table) {
-    DataType dataTypeChildren[2] = {unknown, unknown};
-    int i;
-    for(i = 0; i < 2; i++) {
-        if(tree->child[i]->nodeType == ntRelOp) {
-            dataTypeChildren[i] = RelOpHandler(tree->child[i], table);
-        } else if(tree->child[i]->nodeType == ntOrOp || tree->child[i]->nodeType == ntAndOp) {
-            dataTypeChildren[i] = CmpHandler(tree->child[i], table);
-        } else if(tree->child[i]->nodeType == ntOp || tree->child[i]->nodeType == ntTrueAssign) {
-            dataTypeChildren[i] = OpHandler(tree->child[i], table);
-        } else if(IsUnary(tree->child[i])) {
-            dataTypeChildren[i] = UnaryHandler(tree->child[i], table);
-        } else if(tree->child[i]->nodeType == ntNotOp) {
-            dataTypeChildren[i] = UnaryCmpHandler(tree->child[i], table);
-        } else if (tree->child[i]->nodeType == ntID || tree->child[i]->nodeType == ntCall) {
-            SymbolTableEntry *newEntry = NewEntry(tree->child[0]);
-            if(AddEntryToScope(newEntry, table)) {
-                dataTypeChildren[i] = newEntry->following->node->dataType;
-            }
-        } else {
-            dataTypeChildren[i] = ConstHandler(tree->child[i], table);
-        }
-    }
-    if(dataTypeChildren[0] == dataTypeChildren[1]) {
-        return boolData;
-    } else {
-        printf("%d %s doesnt match %d %s\n", dataTypeChildren[0], tree->child[0]->literal, dataTypeChildren[1], tree->child[1]->literal);
-        return dataTypeChildren[0];
-    }
-}
-
-DataType UnaryCmpHandler (Node * tree, ScopeTable * table) {
-    if(tree->child[0]->nodeType == ntOp || tree->child[0]->nodeType == ntTrueAssign) {
-        return OpHandler(tree->child[0], table);
-    } else if(IsUnary(tree->child[0])) { // is this possible??
-        return UnaryHandler(tree->child[0], table);
-    } else if(tree->child[0]->nodeType == ntID || tree->child[0]->nodeType == ntCall) {
-        SymbolTableEntry *newEntry = NewEntry(tree->child[0]);
-        if(AddEntryToScope(newEntry, table)) {
-            return newEntry->following->node->dataType;
-        }
-    } else {
-        return boolData;
-    }
-}
-
-DataType RelOpHandler (Node * tree, ScopeTable * table) {
-    DataType dataTypeChildren[2] = {unknown, unknown};
-    int i;
-    for(i = 0; i < 2; i++) {
-        if(tree->child[i]->nodeType == ntOp || tree->child[i]->nodeType == ntTrueAssign) {
-            dataTypeChildren[i] = OpHandler(tree->child[i], table);
-        } else if(IsUnary(tree->child[i])) {
-            dataTypeChildren[i] = UnaryHandler(tree->child[i], table);
-        } else if(tree->child[i]->nodeType == ntID || tree->child[i]->nodeType == ntCall) {
-            SymbolTableEntry *newEntry = NewEntry(tree->child[i]);
-            if(AddEntryToScope(newEntry, table)) {
-                dataTypeChildren[i] = newEntry->following->node->dataType;
-            }
-        } else {
-            dataTypeChildren[i] = ConstHandler(tree->child[i], table);
-        }
-    }
-    if(dataTypeChildren[0] == dataTypeChildren[1]) {
-        return boolData;
-    } else {
-        printf("%d %s doesnt match %d %s\n", dataTypeChildren[0], tree->child[0]->literal, dataTypeChildren[1], tree->child[1]->literal);
-        return dataTypeChildren[0];
-    }
-}
-
-DataType OpHandler (Node * tree, ScopeTable * table) {
-    DataType dataTypeChildren[2] = {unknown, unknown};
-    int i;
-    for(i = 0; i < 2; i++) {
-        if(tree->child[i]->nodeType == ntRelOp) {
-            dataTypeChildren[i] = RelOpHandler(tree->child[i], table);
-        } else if(tree->child[i]->nodeType == ntOp || tree->child[i]->nodeType == ntTrueAssign) {
-            dataTypeChildren[i] = OpHandler(tree->child[i], table);
-        } else if(IsUnary(tree->child[i])) {
-            dataTypeChildren[i] = UnaryHandler(tree->child[i], table);
-        } else if(tree->child[i]->nodeType == ntID || tree->child[i]->nodeType == ntCall) {
-            SymbolTableEntry *newEntry = NewEntry(tree->child[i]);
-            if(AddEntryToScope(newEntry, table)) {
-                dataTypeChildren[i] = newEntry->following->node->dataType;
-            }
-        } else {
-            dataTypeChildren[i] = ConstHandler(tree->child[i], table);
-        }
-    }
-    if(dataTypeChildren[0] == unknown || dataTypeChildren[1] == unknown) {
-        return unknown;
-    } else if(dataTypeChildren[0] == dataTypeChildren[1]) {
-        return dataTypeChildren[0];
-    } else {
-        errs = errs + 1;
-        printf(
-            "ERROR(%d): '%s' requires operands of the same type but lhs is type %s and rhs is type %s.\n",
-            tree->lineNum,
-            tree->literal,
-            DataTypeToString(dataTypeChildren[0]),
-            DataTypeToString(dataTypeChildren[1])
-        );
-        return dataTypeChildren[0];
-    }
-}
-
-DataType UnaryHandler (Node * tree, ScopeTable * table) {
-    if(tree->child[0]->nodeType == ntOp || tree->child[0]->nodeType == ntTrueAssign) {
-        return OpHandler(tree->child[0], table);
-    } else if(IsUnary(tree->child[0])) {
-        return UnaryHandler(tree->child[0], table);
-    } else if(tree->child[0]->nodeType == ntID || tree->child[0]->nodeType == ntCall) {
-        SymbolTableEntry *newEntry = NewEntry(tree->child[0]);
-        if(AddEntryToScope(newEntry, table)) {
-            return newEntry->following->node->dataType;
-        }
-    } else {
-        return ConstHandler(tree->child[0], table);
-    }
-}
-
-DataType ConstHandler (Node * tree, ScopeTable * table) {
-    SymbolTableEntry *newEntry = NULL;
-    switch (tree->nodeType) {
-        case ntNumConst:
-            return intData;
-            break;
-        case ntCharConst:
-            return charData;
-            break;
-        case ntStringConst:
-            printf("what do I do with this??\n");
-            break;
-        case ntBoolConst:
-            return boolData;
-            break;
-        case ntArrAd: // a little haphazard no?
-            newEntry = NewEntry(tree->child[0]);
-            AddEntryToScope(newEntry, table);
-            return newEntry->following->node->dataType;
-            break;
-        default:
-            printf("%s\n", tree->literal);
-            break;
-    }
-}
-
 int IsScope (Node * node) {
     if(
-        node->nodeType == ntCompound
-        || node->nodeType == ntFunc
-        || node->nodeType == ntIterwComp
-        || node->nodeType == ntTowComp
+        node->nodeType == ntCompound ||
+        node->nodeType == ntFunc ||
+        node->nodeType == ntIterwComp ||
+        node->nodeType == ntTowComp
+    ) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+int IsNewEntry (Node * node) {
+    if(
+        node->nodeType == ntID ||
+        node->nodeType == ntCall
+    ) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+int IsOp (Node * node) {
+    if(
+        node->nodeType == ntOp ||
+        node->nodeType == ntTrueAssign
+    ) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+int IsCmp (Node * node) {
+    if(
+        node->nodeType == ntOrOp ||
+        node->nodeType == ntAndOp
+    ) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+int IsUnaryCmp (Node * node) {
+    if(
+        node->nodeType == ntNotOp
+    ) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+int IsRelOp (Node * node) {
+    if(
+        node->nodeType == ntRelOp
     ) {
         return 1;
     } else {
@@ -272,6 +254,18 @@ int IsUnary (Node * node) {
         node->nodeType == ntSignOp ||
         node->nodeType == ntSizeofOp ||
         node->nodeType == ntQuestOp
+    ) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+int IsConst (Node * node) {
+    if(
+        node->nodeType == ntNumConst ||
+        node->nodeType == ntCharConst ||
+        node->nodeType == ntBoolConst
     ) {
         return 1;
     } else {
