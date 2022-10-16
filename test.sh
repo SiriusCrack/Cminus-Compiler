3a69c915c0f8b84e1ae7de5badf9b4cb029f203d
SiriusCrack <MorganBrockman1998@Gmail.com>
2022-10-15 19:04:31 -0700

    updated test to check working test dir for HW3/HW4

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
-TESTBATCH=testDataA2
+TESTBATCH=working
 
 for i in ./TestData/$TESTBATCH/*.c-; do
 	echo $i
 	#j=${i##*/}
 	j=${i%.c-}
 	j=$j.out
-	diff -rs <(./src/c- -p $i) $j | egrep '^Files .+ and .+ are identical$' | awk -F '(Files | and | are identical)' '{print "MATCH"}'
+	diff -rs <(./src/c- -P $i) $j | egrep '^Files .+ and .+ are identical$' | awk -F '(Files | and | are identical)' '{print "MATCH"}'
 done
