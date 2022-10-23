acd3c84df688458069ce242895e04a0e139cad28
SiriusCrack <MorganBrockman1998@Gmail.com>
2022-10-23 12:12:46 -0700

    updeted test script

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
-TESTBATCH=working
+TESTBATCH=finished
 
 for i in ./TestData/$TESTBATCH/*.c-; do
 	echo $i
 	#j=${i##*/}
 	j=${i%.c-}
 	j=$j.out
-	diff -rs <(./src/c- -P $i) $j | grep -E '^Files .+ and .+ are identical$' | awk -F '(Files | and | are identical)' '{print "MATCH"}'
+	diff -rs <(sort <(./src/c- -P $i)) <(sort $j) | grep -E '^Files .+ and .+ are identical$' | awk -F '(Files | and | are identical)' '{print "MATCH"}'
 done
