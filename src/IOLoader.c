5707d39adf23e2ff2bd6fb9228fd9976adc5bda8
SiriusCrack <MorganBrockman1998@Gmail.com>
2022-12-11 01:30:41 -0800

    fudged TMAddresses for IO

--- src/IOLoader.c
+++ src/IOLoader.c
@@ -46,27 +46,34 @@ Node * LoadIO () {
 
     tree = fudgeNode("input", ntFunc);
     tree->dataType = intData;
+    tree->tmAddress = 0;
     AddSibling(tree, fudgeNode("output", ntFunc));
     tree->sibling->dataType = voidData;
+    tree->sibling->tmAddress = 5;
     AddChild(tree->sibling, fudgeNode("*dummy*", ntParm));
     tree->sibling->child[0]->dataType = intData;
 
     AddSibling(tree, fudgeNode("inputb", ntFunc));
     tree->sibling->sibling->dataType = boolData;
+    tree->sibling->sibling->tmAddress = 11;
     AddSibling(tree, fudgeNode("outputb", ntFunc));
     tree->sibling->sibling->sibling->dataType = voidData;
+    tree->sibling->sibling->sibling->tmAddress = 16;
     AddChild(tree->sibling->sibling->sibling, fudgeNode("*dummy*", ntParm));
     tree->sibling->sibling->sibling->child[0]->dataType = boolData;
 
     AddSibling(tree, fudgeNode("inputc", ntFunc));
     tree->sibling->sibling->sibling->sibling->dataType = charData;
+    tree->sibling->sibling->sibling->sibling->tmAddress = 22;
     AddSibling(tree, fudgeNode("outputc", ntFunc));
     tree->sibling->sibling->sibling->sibling->sibling->dataType = voidData;
+    tree->sibling->sibling->sibling->sibling->sibling->tmAddress = 27;
     AddChild(tree->sibling->sibling->sibling->sibling->sibling, fudgeNode("*dummy*", ntParm));
     tree->sibling->sibling->sibling->sibling->sibling->child[0]->dataType = charData;
 
     AddSibling(tree, fudgeNode("outnl", ntFunc));
     tree->sibling->sibling->sibling->sibling->sibling->sibling->dataType = voidData;
+    tree->sibling->sibling->sibling->sibling->sibling->sibling->tmAddress = 33;
 
     fudgeTable(tree, SymbolTable);
     tree->dataType = intData;
